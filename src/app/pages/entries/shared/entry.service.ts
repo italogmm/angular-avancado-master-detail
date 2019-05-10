68efc76ef1543ed4416c0975982e46d050782cbe
Italo <italogmm@gmail.com>
2019-05-09 21:18:21 -0300

    finalizando curso
    ..

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,9 +1,10 @@
 import {Injectable, Injector} from '@angular/core';
 import {Observable} from 'rxjs';
-import {catchError, flatMap} from 'rxjs/operators';
+import {catchError, flatMap, map} from 'rxjs/operators';
 import {Entry} from './entry.model';
 import {CategoryService} from '../../categories/shared/category.service';
 import {BaseResourceService} from '../../../shared/services/base-resource.service';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -30,5 +31,22 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handleError)
     );
+  }
+
+  public getByMonthAndYear(month: number, year: number): Observable<Entry[]>{
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+      if(monthMatches && yearMatches){
+        return entry;
+      }
+    });
   }
 }
